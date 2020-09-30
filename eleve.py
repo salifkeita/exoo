d4b0696d655843da8fcb22d24e94c9d52aaabd3d
Salif KEITA <you@example.com>
2020-09-30 00:46:02 +0000

    structure

--- eleve.py
+++ eleve.py
@@ -6,8 +6,18 @@ def info_eleve():
         age = input(f"votre age est: ") 
         classe = input("votre claase : ")
         fichier= open("info.txt","a")
-        fichier.writelines(f"{nom}{prenom}:{age} ans  : {classe} Année \n")
+        fichier.writelines(f"{nom} {prenom} {age}ans {classe} Année \n")
         fichier.close
 
     
+def liste_info():
+    fichier= open("info.txt","r")
+    liste= fichier.readlines()
+    for classe in liste:
+        classe= classe.split(" ")
+        print(f"Nom: {classe [0]}\nPrenom: {classe [1]}\nAge: {classe [2]}\nclasse:{classe [3]} {classe [4]} {classe [5]}")
+    fichier.close
+
+
 info_eleve()
+liste_info()
